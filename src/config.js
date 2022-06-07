773803b81f70a0061daf33b293a3c76f1e554fe5
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-06-07 14:03:45 +0900

    commit

--- src/config.js
+++ src/config.js
@@ -26,13 +26,13 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Back" },
+      { name: "Body" },
+      { name: "Egg" },
+      { name: "Eye" },
+      { name: "hair" },
+      { name: "Idol" },
+      { name: "Mouth" },
     ],
   },
 ];
